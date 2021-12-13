73d5ec5defb713827de8ab4525546d73c724680d
Federico Viceconti <viceconti.federico@gmail.com>
2021-12-13 12:39:45 +0100

    remove analytics

--- lib/utility/firebase_analytics_helper.dart
+++ lib/utility/firebase_analytics_helper.dart
@@ -4,6 +4,8 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
 
 class FirebaseAnalyticsHelper {
+  bool get _initialize => false;
+
   static const EASTER_EGG_FOUND = 'easter_egg_found';
   static const MAIL_HOME_TAP = 'contact_mail_home_tap';
   static const GITHUB_CONTACTS = 'github_contacts';
@@ -28,18 +30,18 @@ class FirebaseAnalyticsHelper {
   FirebaseAnalyticsObserver get observer => _observer;
 
   void logAppOpen() {
-    if(kReleaseMode) _analytics.logAppOpen();
+    if(_initialize) _analytics.logAppOpen();
   }
 
   void logEvent({required String name, Map<String, Object?>? parameters}) {
-    if(kReleaseMode) _analytics.logEvent(name: name, parameters: parameters);
+    if(_initialize) _analytics.logEvent(name: name, parameters: parameters);
   }
 
   Future<void> initialize() async {
-    if(kReleaseMode) await Firebase.initializeApp();
+    if(_initialize) await Firebase.initializeApp();
   }
 
   void setCurrentScreen({required String screenName}) {
-    if(kReleaseMode) _analytics.setCurrentScreen(screenName: screenName);
+    if(_initialize) _analytics.setCurrentScreen(screenName: screenName);
   }
 }
